b946d147eadcea45404100c67ae677a79123d7b0
Christian Adell <chadell@gmail.com>
2023-04-23 07:39:33 +0200

    add checkerror (#11)

--- ch10-apis/go_http/get_config_interface_g1.go
+++ ch10-apis/go_http/get_config_interface_g1.go
@@ -1,32 +1,40 @@
 package main
 
 import (
-  "fmt"
-  "encoding/base64"
-  "io/ioutil"
-  "net/http"
-  "crypto/tls"
+	"crypto/tls"
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 func basicAuth(username, password string) string {
-  auth := username + ":" + password
-  return base64.StdEncoding.EncodeToString([]byte(auth))
+	auth := username + ":" + password
+	return base64.StdEncoding.EncodeToString([]byte(auth))
+}
+
+// helper method to implement the error-checking pattern
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
-  transCfg := &http.Transport{
-    TLSClientConfig: &tls.Config{InsecureSkipVerify: true}, // ignore expired SSL certificates
-  }
-  client := &http.Client{Transport: transCfg}
-  request, _ := http.NewRequest("GET", "https://csr1/restconf/data/ietf-interfaces:interfaces/interface=GigabitEthernet1", nil)
-  request.Header.Set("Accept", "application/yang-data+json")
-  request.Header.Add("Authorization","Basic " + basicAuth("ntc","ntc123"))
+	transCfg := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true}, // ignore expired SSL certificates
+	}
+	client := &http.Client{Transport: transCfg}
+	request, err := http.NewRequest("GET", "https://csr1/restconf/data/ietf-interfaces:interfaces/interface=GigabitEthernet1", nil)
+	checkError(err)
+	request.Header.Set("Accept", "application/yang-data+json")
+	request.Header.Add("Authorization", "Basic "+basicAuth("ntc", "ntc123"))
 
-  result, err := client.Do(request)
-  if (err!=nil){
-    fmt.Printf("%s",err)
-  }
-  body, _ := ioutil.ReadAll(result.Body)
-  result.Body.Close()
-  fmt.Printf("%s", body)
+	result, err := client.Do(request)
+	checkError(err)
+	body, err := ioutil.ReadAll(result.Body)
+	checkError(err)
+	result.Body.Close()
+	fmt.Printf("%s", body)
 }
