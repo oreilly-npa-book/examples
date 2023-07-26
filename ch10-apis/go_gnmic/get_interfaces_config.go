ecff499eff00b050ab202acba4ef9010263ec4e8
Christian Adell <chadell@gmail.com>
2023-07-26 16:31:49 +0200

    QC1 review (#12)
    * Rename Master to Main
    
    * Remove reference to appendix
    
    * update prefixes
    
    * replace karima org by openconfig for gnmic

--- ch10-apis/go_gnmic/get_interfaces_config.go
+++ ch10-apis/go_gnmic/get_interfaces_config.go
@@ -1,48 +1,48 @@
 package main
 
 import (
-  "context"
-  "fmt"
-  "log"
+	"context"
+	"fmt"
+	"log"
 
-  "github.com/karimra/gnmic/api"
-  "google.golang.org/protobuf/encoding/prototext"
+	"github.com/openconfig/gnmic/api"
+	"google.golang.org/protobuf/encoding/prototext"
 )
 
 func check_error(err error) {
-  if err != nil {
-    log.Fatal(err)
-  }
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
-  // create a target
-  tg, err := api.NewTarget(
-      api.Name("gnmi example"),
-      api.Address("clab-demo-eos-spine1:6030"),
-      api.Username("admin"),
-      api.Password("admin"),
-      api.Insecure(true),
-  )
-  check_error(err)
+	// create a target
+	tg, err := api.NewTarget(
+		api.Name("gnmi example"),
+		api.Address("clab-demo-eos-spine1:6030"),
+		api.Username("admin"),
+		api.Password("admin"),
+		api.Insecure(true),
+	)
+	check_error(err)
 
-  ctx, cancel := context.WithCancel(context.Background())
-  defer cancel()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-  // create a gNMI client
-  err = tg.CreateGNMIClient(ctx)
-  check_error(err)
-  defer tg.Close()
+	// create a gNMI client
+	err = tg.CreateGNMIClient(ctx)
+	check_error(err)
+	defer tg.Close()
 
-  // create a GetRequest
-  getReq, err := api.NewGetRequest(
-      api.Path("/interfaces/interface/config"),
-      api.Encoding("json_ietf"))
-  check_error(err)
-  fmt.Println(prototext.Format(getReq))
+	// create a GetRequest
+	getReq, err := api.NewGetRequest(
+		api.Path("/interfaces/interface/config"),
+		api.Encoding("json_ietf"))
+	check_error(err)
+	fmt.Println(prototext.Format(getReq))
 
-  // send the created gNMI GetRequest to the created target
-  getResp, err := tg.Get(ctx, getReq)
-  check_error(err)
-  fmt.Println(prototext.Format(getResp))
+	// send the created gNMI GetRequest to the created target
+	getResp, err := tg.Get(ctx, getReq)
+	check_error(err)
+	fmt.Println(prototext.Format(getResp))
 }
