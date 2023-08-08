4908f6a88f0f607eca4ede4b69309e992cb28a47
Christian Adell <chadell@gmail.com>
2023-08-08 22:55:57 +0200

    add other method

--- ch06-python/vendors/cisco/device.py
+++ ch06-python/vendors/cisco/device.py
@@ -3,3 +3,6 @@ class Device:
         self.host_ip = ip
         self.user = username
         self.pswd = password
+
+    def show(self, command):
+        return f'{command} output from {self.host_ip}'
